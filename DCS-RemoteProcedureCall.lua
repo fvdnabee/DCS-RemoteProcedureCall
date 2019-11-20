e8cea674f74b28c8b5f893ca1169589c5af7c2c3
Floris <floris@vdna.be>
2019-11-20 21:08:49 +0100

    Update RPC script to reply on client socket and support RPC responses Also added PING/PONG mechanism

--- DCS-RemoteProcedureCall.lua
+++ DCS-RemoteProcedureCall.lua
@@ -23,7 +23,7 @@ dcsrpc.UDPReceiveSocket = socket.udp()
 dcsrpc.UDPReceiveSocket:setsockname("*", dcsrpc.RECEIVE_PORT)
 dcsrpc.UDPReceiveSocket:settimeout(0) --receive timer was 0001
 
--- send response
+-- send response to multiple clients ('broadcast')
 dcsrpc.UDPSendSocket = socket.udp()
 dcsrpc.UDPSendSocket:settimeout(0)
 
@@ -33,56 +33,63 @@ local _lastReceivedCheck = 0;
 dcsrpc.onSimulationFrame = function()
 
     local _now = DCS.getRealTime()
-	
+
 	-- check every 1 second if we received a new message:
     if _now > _lastReceivedCheck + 1.0 then
         _lastReceivedCheck = _now 
         net.log("DCSRPC - checking UDP socket for new messages")
-		
+
 		-- read from socket
 		local _status, _result = pcall(function()
 
-			local _received = dcsrpc.UDPReceiveSocket:receive()
+			-- local _received = dcsrpc.UDPReceiveSocket:receive()
+			_received, _msg_or_ip, _port_or_nil = dcsrpc.UDPReceiveSocket:receivefrom()
 
 			if _received then
+				net.log("DCSRPC - Received message from ".. _msg_or_ip .. ":" .. _port_or_nil)
+				if _port_or_nil == nil then -- Fallback to general send port:
+					net.log("DCSRPC - _port_or_nil is nil, falling back to default send port: " .. dcsrpc.SEND_PORT)
+					_port_or_nil = dcsrpc.SEND_PORT
+				end
+
 				if string.len(_received) >= 2 then
 					if _received:sub(1,1) == "!" then  -- RPC received
-						dcsrpc.handleRPC(_received)
+						returncode, response = dcsrpc.handleRPC(_received)
+
+						-- send response over socket:
+						net.log("DCSRPC - sending RPC response with rc = " .. returncode .." over UDP socket")
+						socket.try(dcsrpc.UDPReceiveSocket:sendto(returncode .. "|" .. response .." \n", _msg_or_ip, _port_or_nil))
+					elseif _received:sub(1,4) == "PING" then  -- PING received, reply with PONG (note we don't consider this an RPC request)
+						net.log("DCSRPC - received PING, sending PONG response over UDP socket")
+						socket.try(dcsrpc.UDPReceiveSocket:sendto("PONG\n", _msg_or_ip, _port_or_nil))
 					end
 				end
-				
-				-- echo incoming message
-				socket.try(dcsrpc.UDPSendSocket:sendto("Received:" .. _received .. " \n", "127.0.0.1", dcsrpc.SEND_PORT))
 			end
 		end)
-		
-		if not _status then
-			net.log('ERROR onSimulationFrame DCSRPC: ' .. _result)
-		end
-    end
 
-    -- send a PING every 5 seconds
-    if _now > _lastSent + 5.0 then
-        _lastSent = _now 
-        net.log("sending update")
-        socket.try(dcsrpc.UDPSendSocket:sendto("PING \n", "127.0.0.1", dcsrpc.SEND_PORT))
+		if not _status then
+			net.log('DCSRPC - ERROR onSimulationFrame: ' .. _result)
+		end
     end
 end
 
 dcsrpc.handleRPC = function(_received)
-	net.log("received RPC call".._received)
+	-- Hanlders in this function should return two values: returncode and a response. 
+	-- A 0 return code indicates succes, non-zero return codes are used to signal different error states.
+	net.log("DCSRPC - received RPC call: ".._received)
 
 	if _received:sub(1,2) == "!N" then  -- send notification
 		local _notificationText = _received:sub(3, -2)  -- remove \n char, _notificationText should not contain any double quotes " as this will interfer with dostring_in below, TODO: check/enforce this
-		net.log("Triggering outText: \"".._notificationText.."\"")
+		net.log("DCSRPC - Triggering outText: \"".._notificationText.."\"")
 
 		local _status, _error = net.dostring_in('server', " return trigger.action.outText(\"".._notificationText.."\", 10); ")
 
 		if not _status and _error then
 			net.log("DCSRPC - error on trigger action outText: ".._error)
-			-- return false
+			return 1, "NOK"
 		else
 			net.log("DCSRPC - trigger.action.outText completed succesfully")
+			return 0, "OK"
 		end
 	end
 end
